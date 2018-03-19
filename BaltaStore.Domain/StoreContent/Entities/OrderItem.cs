2ee6a5018c7ff903a16a2fd6613f40a359b8d223
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-03-19 13:15:16 -0300

    17 - SIMULANDO O PEDIDO

--- BaltaStore.Domain/StoreContent/Entities/OrderItem.cs
+++ BaltaStore.Domain/StoreContent/Entities/OrderItem.cs
@@ -2,7 +2,7 @@
 {
     public class OrderItem
     {
-        public OrderItem(Product product, string quantity)
+        public OrderItem(Product product, decimal quantity)
         {
             Product = product;
             Quantity = quantity;
@@ -10,7 +10,7 @@
         }
 
         public Product Product { get; private set; }
-        public string Quantity { get; private set; }
+        public decimal Quantity { get; private set; }
         public decimal Price { get; private set; }
     }
 }
--- BaltaStore.Tests/UnitTest1.cs
+++ BaltaStore.Tests/UnitTest1.cs
@@ -10,12 +10,29 @@ namespace BaltaStore.Tests
         [TestMethod]
         public void TestMethod1()
         {
-            var customer = new Customer( 
-                new Name("George","Bonespírito"),
-                new Document("12312213"), 
-                new Email("torgge@gmail.com"), 
+            var customer = new Customer(
+                new Name("George", "Bonespírito"),
+                new Document("12312213"),
+                new Email("torgge@gmail.com"),
                 "1312312312");
-            customer.ToString();
+
+            var mouse = new Product("Mouse", "Mouse Óptico", "img.png", 12.50M, 12);
+            var teclado = new Product("Teclado", "Teclado Mecânico", "img.png", 46.54M, 122);
+            var monitor = new Product("Monitor", "Monitor VGA", "img.png", 12.50M, 1);
+
+            var order = new Order(customer);
+            order.AddItem(new OrderItem(mouse, 20));
+            order.AddItem(new OrderItem(teclado, 10));
+            order.AddItem(new OrderItem(monitor, 40));
+            
+            //Realizei o Pedido.
+            order.Place();
+            
+            order.Pay();
+            
+            order.Ship();
+            
+            order.Cancel();
         }
     }
 }
