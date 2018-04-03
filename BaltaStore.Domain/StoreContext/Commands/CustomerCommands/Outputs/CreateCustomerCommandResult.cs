71b32e094b1178f647ae1c14db3ff356f8d6d0f7
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-03 12:47:30 -0400

    33 - CRIANDO UM HANDLER.

---
+++ BaltaStore.Domain/StoreContext/Commands/CustomerCommands/Outputs/CreateCustomerCommandResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+using BaltaStore.Shared.Commands;
+
+namespace BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Outputs
+{
+    public class CreateCustomerCommandResult : ICommandResult
+    {
+        public CreateCustomerCommandResult()
+        {
+        }
+
+        public CreateCustomerCommandResult(Guid id, string firsName, string email)
+        {
+            Id = id;
+            FirsName = firsName;
+            Email = email;
+        }
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public Guid Id { get; set; }
+        public string FirsName { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
---
+++ BaltaStore.Domain/StoreContext/Handlers/CustomerHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Outputs;
+using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.ValueObjects;
+using BaltaStore.Shared.Commands;
+using FluentValidator;
+
+namespace BaltaStore.Domain.StoreContext.Handlers
+{
+    public class CustomerHandler : Notifiable, 
+        ICommandHandler<CreateCustomerCommand>, 
+        ICommandHandler<AddAddressCommand>
+    {
+        public ICommandResult Handle(CreateCustomerCommand command)
+        {
+            //Veririficar se cpf já existe na base
+            
+            //Verificar se o email já existe na base
+            
+            //Criar os VO's
+            var name = new Name(command.FirstName, command.LastName);
+            var document = new Document(command.Document);
+            var email = new Email(command.Email);
+            
+            //Criar a entidade
+            var customer = new Customer(name, document, email, command.Phone);
+            
+            //Validar entidades e VOs
+            AddNotifications(name.Notifications);
+            AddNotifications(document.Notifications);
+            AddNotifications(email.Notifications);
+            AddNotifications(customer.Notifications);
+            
+            //Persistir o Cliente
+            
+            //Enviar email de boas vindas
+            
+            //Retornar o resultado para a tela
+            return new CreateCustomerCommandResult(Guid.NewGuid(), name.ToString(), email.Address);
+        }
+
+        public ICommandResult Handle(AddAddressCommand command)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
