e134dfb21192a0f3f24298cb46b92bc9f5788c64
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-03 12:23:35 -0400

    31 - Testando Commands.

---
+++ BaltaStore.Tests/Commands/CreateCustomerCommandTests.cs
@@ -0,0 +1,25 @@
+﻿using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BaltaStore.Tests.Commands
+{
+    [TestClass]
+    public class CreateCustomerCommandTests
+    {
+
+        [TestMethod]
+        public void ShouldValidateWhenCommandIsValid()
+        {
+            var command = new CreateCustomerCommand();
+
+            command.FirstName = "George";
+            command.LastName = "Bonespírito";
+            command.Document = "123123123123";
+            command.Email = "torgge@gmail.com";
+            command.Phone = "12312313131";
+            
+            Assert.AreEqual(false, command.Valid());
+        }
+        
+    }
+}
