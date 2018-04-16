d02affd487fefeb2cfb1ff48b799182eb8bbcf2d
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-16 12:57:48 -0400

    41 - CRIANDO O DATA CONTEXT.

---
+++ BaltaStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace BaltaStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=.\sqlexpress;Database=baltastore;User ID=SA;Password=ervamate@00;";
+    }
+}
