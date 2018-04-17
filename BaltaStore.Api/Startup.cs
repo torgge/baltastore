f18f687f2312675cf9a00b163be0fdf234e8282b
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-17 13:03:25 -0400

    44 - INICIANDO O PROJETO WEB

--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BaltaStore.Api
@@ -21,14 +16,7 @@ namespace BaltaStore.Api
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
