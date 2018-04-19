54b24208bbbaa766058a4fdaae3f0c859479d43b
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-19 12:47:48 -0400

    46 - CRIANDO UM CONTROLLER.

--- BaltaStore.Api/Controllers/HomeController.cs
+++ BaltaStore.Api/Controllers/HomeController.cs
@@ -4,8 +4,30 @@ namespace BaltaStore.Api.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         [Route("")]
         public object Get()
+        {
+            return new {version = "version 0.0.0.1"};
+        }
+        
+        [HttpPost]
+        [Route("")]
+        public object Post()
+        {
+            return new {version = "version 0.0.0.1"};
+        }
+        
+        [HttpPut]
+        [Route("")]
+        public object Put()
+        {
+            return new {version = "version 0.0.0.1"};
+        }
+        
+        [HttpDelete]
+        [Route("")]
+        public object Delete()
         {
             return new {version = "version 0.0.0.1"};
         }
