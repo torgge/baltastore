2d74f03c45a766ddcad520851e8486d4575236a4
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-19 12:50:52 -0400

    48 - CRUD.

---
+++ BaltaStore.Api/Controllers/CustomerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using BaltaStore.Domain.StoreContext.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaltaStore.Api.Controllers
+{
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        [Route("cutomers")]
+        public List<Customer> Get()
+        {
+            return null;
+        }
+        
+        [HttpPost]
+        [Route("customers")]
+        public Customer Post([FromBody] Customer customer)
+        {
+            return customer;
+        }
+        
+        [HttpPut]
+        [Route("customers/{id}")]
+        public Customer Put([FromBody] Customer customer)
+        {
+            return customer;
+        }
+        
+        [HttpDelete]
+        [Route("customers/{id}")]
+        public Customer Delete()
+        {
+            return null;
+        }
+        
+        [HttpGet]
+        [Route("cutomer/{id}")]
+        public Customer GetById(Guid id)
+        {
+            return null;
+        }
+        
+        [HttpGet]
+        [Route("orders")]
+        public List<Order> GetOrders()
+        {
+            return null;
+        }
+        
+        
+        
+    }
+}
