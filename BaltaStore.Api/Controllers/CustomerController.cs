f2b26837518eb38d27f51adac958c1436d5abef7
GEO-DATAPAR\george.bonespirito <george.bonespirito@datapar.com.py>
2018-04-23 12:18:21 -0400

    52 - UTILIZANDO OS HANDLERS

--- BaltaStore.Api/Controllers/CustomerController.cs
+++ BaltaStore.Api/Controllers/CustomerController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Inputs;
+using BaltaStore.Domain.StoreContext.Commands.CustomerCommands.Outputs;
 using BaltaStore.Domain.StoreContext.Entities;
+using BaltaStore.Domain.StoreContext.Handlers;
 using BaltaStore.Domain.StoreContext.Queries;
 using BaltaStore.Domain.StoreContext.Repositories;
 using BaltaStore.Domain.StoreContext.ValueObjects;
+using BaltaStore.Shared.Commands;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BaltaStore.Api.Controllers
@@ -12,10 +15,12 @@ namespace BaltaStore.Api.Controllers
     public class CustomerController : Controller
     {
         private readonly ICustomeRepository _repository;
+        private readonly CustomerHandler _customerHandler;
 
-        public CustomerController(ICustomeRepository repository)
+        public CustomerController(ICustomeRepository repository, CustomerHandler customerHandler)
         {
             _repository = repository;
+            _customerHandler = customerHandler;
         }
 
         [HttpGet]
@@ -27,14 +32,13 @@ namespace BaltaStore.Api.Controllers
         
         [HttpPost]
         [Route("customers")]
-        public Customer Post([FromBody] CreateCustomerCommand command)
+        public object Post([FromBody] CreateCustomerCommand command)
         {
-            var name = new Name(command.FirstName, command.LastName);
-            var document = new Document(command.Document);
-            var email = new Email(command.Email);
-            var customer = new Customer(name, document, email, command.Phone);
-
-            return customer;
+            var result = (CreateCustomerCommandResult)_customerHandler.Handle(command);
+            if (_customerHandler.Invalid)
+                return BadRequest(_customerHandler.Notifications);
+            
+            return result;
         }
         
         [HttpPut]
--- BaltaStore.Api/Startup.cs
+++ BaltaStore.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using BaltaStore.Domain.StoreContext.Repositories;
+﻿using BaltaStore.Domain.StoreContext.Handlers;
+using BaltaStore.Domain.StoreContext.Repositories;
 using BaltaStore.Domain.StoreContext.Services;
 using BaltaStore.Infra.StoreContext.DataContexts;
 using BaltaStore.Infra.StoreContext.EmailService;
@@ -20,6 +21,7 @@ namespace BaltaStore.Api
             services.AddScoped<BaltaDataContext, BaltaDataContext>();
             services.AddTransient<ICustomeRepository, CustomerRepository>();
             services.AddTransient<IEmailService, EmailService>();
+            services.AddTransient<CustomerHandler, CustomerHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
